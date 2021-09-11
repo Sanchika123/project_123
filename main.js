0c64c85ee015911178e28e8633a1fa76db7e6037
Sanchika123 <79789552+Sanchika123@users.noreply.github.com>
2021-09-11 07:07:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
 function setup(){
     video= createCapture(VIDEO);
     video.size(550, 550);
@@ -15,9 +18,19 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log('leftWristX: '+leftWristX+' rightWristX: '+rightWristX+' difference: '+difference);
+        document.getElementById('text_size').innerHTML= 'The text size will be: '+difference;
     }
 }
 
 function draw(){
     background('#5dba8f');
+
+    textSize(difference);
+    fill('#4287f5');
+    text('Sanchika', 40, 500);
 }
+
